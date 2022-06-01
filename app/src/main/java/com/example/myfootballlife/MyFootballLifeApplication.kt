3b7602e1b2c161536f6c6e8b8f804eddd364b55a
dogeby <dogeby@gmail.com>
2022-06-01 22:49:47 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myfootballlife/MyFootballLifeApplication.kt
@@ -0,0 +1,8 @@
+package com.example.myfootballlife
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyFootballLifeApplication: Application() {
+}
