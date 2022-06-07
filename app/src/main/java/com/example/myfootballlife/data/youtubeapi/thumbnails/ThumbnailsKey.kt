e213d84238cab9322f066a97aa442cd567f32956
dogeby <dogeby@gmail.com>
2022-06-08 05:22:35 +0900

    ThumbnailsKey.kt 변수명 대문자로 변경

--- app/src/main/java/com/example/myfootballlife/data/youtubeapi/thumbnails/ThumbnailsKey.kt
+++ app/src/main/java/com/example/myfootballlife/data/youtubeapi/thumbnails/ThumbnailsKey.kt
@@ -2,7 +2,7 @@ package com.example.myfootballlife.data.youtubeapi.thumbnails
 
 /** https://developers.google.com/youtube/v3/docs/thumbnails?hl=ko */
 object ThumbnailsKey {
-    const val default = "default"
-    const val medium = "medium"
-    const val high = "high"
+    const val DEFAULT = "default"
+    const val MEDIUM = "medium"
+    const val HIGH = "high"
 }
