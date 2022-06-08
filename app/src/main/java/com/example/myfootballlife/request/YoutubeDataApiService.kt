2086c544637e00475e90b2d4eedcc724b3e06318
dogeby <dogeby@gmail.com>
2022-06-08 21:34:42 +0900

    YoutubeDataApiKey local.properties에 숨기기

--- app/src/main/java/com/example/myfootballlife/request/YoutubeDataApiService.kt
+++ app/src/main/java/com/example/myfootballlife/request/YoutubeDataApiService.kt
@@ -1,5 +1,6 @@
 package com.example.myfootballlife.request
 
+import com.example.myfootballlife.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,26 +11,26 @@ import retrofit2.http.Query
 interface YoutubeDataApiService {
 
     companion object {
-        private const val apiKey = "AIzaSyAcgdr4Edx9dE-2o1hIsO7D74CPmYfxARY"
+        private const val YOUTUBE_DATA_API_KEY = "${BuildConfig.YOUTUBE_DATA_API_KEY}"
     }
 
     /** https://developers.google.com/youtube/v3/docs/channels/list **/
     @GET("channels")
-    suspend fun requestListChannels(@Query("part")part: String, @Query("forUsername") forUsername:String? = null, @Query("id") id:String? = null, @Query("key") key: String = apiKey):
+    suspend fun requestListChannels(@Query("part")part: String, @Query("forUsername") forUsername:String? = null, @Query("id") id:String? = null, @Query("key") key: String = YOUTUBE_DATA_API_KEY):
             com.example.myfootballlife.data.youtubeapi.channels.ResponseBody
 
     /** https://developers.google.com/youtube/v3/docs/playlists/list */
     @GET("playlists")
-    suspend fun requestListPlaylists(@Query("part")part: String, @Query("id") id:String? = null, @Query("channelId") channelId:String? = null, @Query("key") key: String = apiKey):
+    suspend fun requestListPlaylists(@Query("part")part: String, @Query("id") id:String? = null, @Query("channelId") channelId:String? = null, @Query("key") key: String = YOUTUBE_DATA_API_KEY):
             com.example.myfootballlife.data.youtubeapi.playlists.ResponseBody
 
     /** https://developers.google.com/youtube/v3/docs/playlistItems/list?hl=ko */
     @GET("playlistItems")
-    suspend fun requestListPlaylistItems(@Query("part")part: String, @Query("id") id:String? = null, @Query("playlistId") playlistId:String? = null, @Query("key") key: String = apiKey):
+    suspend fun requestListPlaylistItems(@Query("part")part: String, @Query("id") id:String? = null, @Query("playlistId") playlistId:String? = null, @Query("key") key: String = YOUTUBE_DATA_API_KEY):
             com.example.myfootballlife.data.youtubeapi.playlistitems.ResponseBody
 
     /** https://developers.google.com/youtube/v3/docs/videos/list?hl=ko */
     @GET("videos")
-    suspend fun requestListVideos(@Query("part")part: String, @Query("id") id:String? = null, @Query("key") key: String = apiKey):
+    suspend fun requestListVideos(@Query("part")part: String, @Query("id") id:String? = null, @Query("key") key: String = YOUTUBE_DATA_API_KEY):
             com.example.myfootballlife.data.youtubeapi.videos.ResponseBody
 }
