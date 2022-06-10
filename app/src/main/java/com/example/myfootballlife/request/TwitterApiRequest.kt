3ede35130c3ecb41ca2d0de160cd86246717d0d8
dogeby <dogeby@gmail.com>
2022-06-11 07:09:09 +0900

    API request class 삭제

--- app/src/main/java/com/example/myfootballlife/request/TwitterApiRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myfootballlife.request
-
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class TwitterApiRequest @Inject constructor(
-    val retrofit:TwitterApiService
-)
--- app/src/main/java/com/example/myfootballlife/request/YoutubeDataApiRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myfootballlife.request
-
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class YoutubeDataApiRequest @Inject constructor(
-    val retrofit:YoutubeDataApiService
-)
