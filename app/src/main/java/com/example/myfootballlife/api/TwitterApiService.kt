55c192963a5ad3630928bb59185985f40df6e3bf
dogeby <dogeby@gmail.com>
2022-06-16 07:37:43 +0900

    TwitterApiService.kt ids로 Users검색 추가, requestUserTweetTimeline query 추가

--- app/src/main/java/com/example/myfootballlife/api/TwitterApiService.kt
+++ app/src/main/java/com/example/myfootballlife/api/TwitterApiService.kt
@@ -20,10 +20,15 @@ interface TwitterApiService {
     /** https://developer.twitter.com/en/docs/twitter-api/tweets/timelines/api-reference/get-users-id-tweets **/
     @GET("2/users/{id}/tweets")
     @Headers("Authorization: Bearer $TWITTER_API_BEARER_TOKEN")
-    suspend fun requestUserTweetTimeline(@Path("id") id:String, @Query("pagination_token") paginationToken:String? = null, @Query("tweet.fields") tweetFields:String = "${TweetsFields.AUTHOR_ID},${TweetsFields.CREATED_AT},${TweetsFields.LANG}"): TimelinesResponseBody
+    suspend fun requestUserTweetTimeline(@Path("id") id:String, @Query("pagination_token") paginationToken:String? = null, @Query("since_id") sinceId:String? = null, @Query("end_time") endTime:String? = null, @Query("tweet.fields") tweetFields:String = "${TweetsFields.AUTHOR_ID},${TweetsFields.CREATED_AT},${TweetsFields.LANG}"): TimelinesResponseBody
 
     /** https://developer.twitter.com/en/docs/twitter-api/users/lookup/api-reference/get-users-by-username-username */
     @GET("2/users/by")
     @Headers("Authorization: Bearer $TWITTER_API_BEARER_TOKEN")
     suspend fun requestRetrieveUsersByUsernames(@Query("usernames") usernames:String, @Query("user.fields") userFields:String = "${UsersFields.CREATED_AT},${UsersFields.DESCRIPTION},${UsersFields.PROFILE_IMAGE_URL}"): UsersResponseBody
+
+    /** https://developer.twitter.com/en/docs/twitter-api/users/lookup/api-reference/get-users **/
+    @GET("2/users")
+    @Headers("Authorization: Bearer $TWITTER_API_BEARER_TOKEN")
+    suspend fun requestUsersByIds(@Query("ids") ids: String, @Query("user.fields") userFields:String = "${UsersFields.CREATED_AT},${UsersFields.DESCRIPTION},${UsersFields.PROFILE_IMAGE_URL}"): UsersResponseBody
 }
