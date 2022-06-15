f59f295ea38830d6e98a5faf0c5553ce37eea3e5
dogeby <dogeby@gmail.com>
2022-06-15 09:05:45 +0900

    Videos.kt 오타 수정

--- app/src/main/java/com/example/myfootballlife/data/youtubeapi/videos/Videos.kt
+++ app/src/main/java/com/example/myfootballlife/data/youtubeapi/videos/Videos.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 /** https://developers.google.com/youtube/v3/docs/videos?hl=ko */
 data class Videos(
-    @SerializedName("name") val id:String,
+    @SerializedName("id") val id:String,
     @SerializedName("snippet") val snippet:Snippet,
     @SerializedName("contentDetails") val contentDetails:ContentDetails,
     @SerializedName("statistics") val statistics:Statistics
