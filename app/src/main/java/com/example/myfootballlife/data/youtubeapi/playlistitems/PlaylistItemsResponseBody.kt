f7c7c175bcd36e7036737fffd02a056736f4c809
dogeby <dogeby@gmail.com>
2022-06-16 10:04:23 +0900

    PlaylistItemsResponseBody.kt token Nullable로 수정

--- app/src/main/java/com/example/myfootballlife/data/youtubeapi/playlistitems/PlaylistItemsResponseBody.kt
+++ app/src/main/java/com/example/myfootballlife/data/youtubeapi/playlistitems/PlaylistItemsResponseBody.kt
@@ -7,8 +7,8 @@ import com.google.gson.annotations.SerializedName
 data class PlaylistItemsResponseBody(
     @SerializedName("kind") val kind:String,
     @SerializedName("etag") val etag:String,
-    @SerializedName("nextPageToken") val nextPageToken: String,
-    @SerializedName("prevPageToken") val prevPageToken: String,
+    @SerializedName("nextPageToken") val nextPageToken: String?,
+    @SerializedName("prevPageToken") val prevPageToken: String?,
     @SerializedName("pageInfo") val pageInfo: PageInfo,
     @SerializedName("items") val items: List<PlaylistItems>
 )
